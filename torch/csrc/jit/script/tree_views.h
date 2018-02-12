eecec783f2c3f082cd9cdb535e14e5bbaed11912
Adam Paszke <adam.paszke@gmail.com>
2018-02-12 12:29:35 -0500

    Add Python frontend to the JIT
    Its main functionality is translating the Python AST into the
    TorchScript AST. It doesn't do any type checking.

--- torch/csrc/jit/script/tree_views.h
+++ torch/csrc/jit/script/tree_views.h
@@ -2,6 +2,8 @@
 #include "error_report.h"
 #include "tree.h"
 
+#include <functional>
+
 namespace torch {
 namespace jit {
 namespace script {
@@ -476,6 +478,9 @@ struct BinOp : public Expr {
   Expr rhs() const {
     return Expr(subtree(1));
   }
+  static BinOp create(const SourceRange& range, int kind, const Expr& lhs, const Expr& rhs) {
+    return BinOp(Compound::create(kind, range, {lhs, rhs}));
+  }
 };
 
 struct UnaryOp : public Expr {
@@ -489,6 +494,9 @@ struct UnaryOp : public Expr {
       default:
         throw ErrorReport(tree) << kindToString(tree->kind()) << " is not a valid UnaryOp";
     }
+  }
+  static UnaryOp create(const SourceRange& range, int kind, const Expr& expr) {
+    return UnaryOp(Compound::create(kind, range, {expr}));
   }
 };
 
@@ -598,12 +606,33 @@ struct Var : public Expr {
   explicit Var(const TreeRef& tree) : Expr(tree) {
     tree_->match(TK_VAR);
   };
-  Ident name() {
+  Ident name() const {
     return Ident(subtree(0));
   }
   static Var create(const SourceRange& range, const Ident& name) {
     return Var(Compound::create(TK_VAR, range, {name}));
   }
+};
+
+struct TernaryIf : public Expr {
+  explicit TernaryIf(const TreeRef& tree) : Expr(tree) {
+    tree_->matchNumSubtrees(TK_IF_EXPR, 3);
+  };
+  Expr cond() const {
+    return Expr(subtree(0));
+  }
+  Expr true_expr() const {
+    return Expr(subtree(1));
+  }
+  Expr false_expr() const {
+    return Expr(subtree(2));
+  }
+  static TernaryIf create(const SourceRange& range,
+                          const Expr& cond,
+                          const Expr& true_expr,
+                          const Expr& false_expr) {
+    return TernaryIf(Compound::create(TK_IF_EXPR, range, {cond, true_expr, false_expr}));
+  };
 };
 
 } // namespace script
